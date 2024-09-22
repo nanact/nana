2310773562db2da22e9289eaba562aa898328a5b
nanact <144712705+nanact@users.noreply.github.com>
2024-09-22 18:02:47 +0000

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+num = float(input("Enter the value num 1"))
+
+num1 = float(input("Enter the value of num2"))
+
+opa = input("enter Oparator")
+
+if opa == "+":
+    print(num1 % num)
+elif opa == "-":
+    print(num - num1)
+elif opa == "x":
+    print(num1 * num)
+elif opa == "/":
+    print(num / num1)
+else:
+    print("The is a error")
+    
